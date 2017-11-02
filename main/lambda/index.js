141d6c94ac473afc8fec8cd6552f4bd8ed53832c
Mark Greene <mark.greene@lexmark.com>
2017-11-02 16:14:23 -0400

    added api gateway

--- main/lambda/index.js
+++ main/lambda/index.js
@@ -3,5 +3,11 @@
 exports.handler = function(event, context, callback) {
     console.log('Event: ', JSON.stringify(event, null, '\t'));
     console.log('Context: ', JSON.stringify(context, null, '\t'));
-    callback(null);
+    callback(null, {
+      statusCode: '200',
+      body: JSON.stringify({ 'message': 'hello world' }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
 };
--- main/main.tf
+++ main/main.tf
@@ -31,25 +31,12 @@ resource "aws_lambda_function" "lambda_demo" {
           TEST = "${data.aws_ssm_parameter.secret_read.value}"
        }
     }
+    publish = true
 }
 
 resource "aws_iam_role" "lambda_exec_role" {
   name = "lambda_exec_role"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+  assume_role_policy = "${file("policies/lambda-role.json")}"
 }
 
 data "archive_file" "lambda_zip" {
@@ -57,6 +44,61 @@ data "archive_file" "lambda_zip" {
     source_dir  = "lambda"
     output_path = "function.zip"
 }
+
+resource "aws_api_gateway_rest_api" "lambda_demo_api" {
+  name = "lambda_demo_api"
+  description = "Lambda Demo Rest Api"
+}
+
+resource "aws_api_gateway_resource" "lambda_demo_api_resource" {
+  rest_api_id = "${aws_api_gateway_rest_api.lambda_demo_api.id}"
+  parent_id = "${aws_api_gateway_rest_api.lambda_demo_api.root_resource_id}"
+  path_part = "messages"
+}
+
+resource "aws_api_gateway_method" "lambda_demo_api_method" {
+  rest_api_id = "${aws_api_gateway_rest_api.lambda_demo_api.id}"
+  resource_id = "${aws_api_gateway_resource.lambda_demo_api_resource.id}"
+  http_method = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_demo_api_method-integration" {
+  rest_api_id = "${aws_api_gateway_rest_api.lambda_demo_api.id}"
+  resource_id = "${aws_api_gateway_resource.lambda_demo_api_resource.id}"
+  http_method = "${aws_api_gateway_method.lambda_demo_api_method.http_method}"
+  type = "AWS_PROXY"
+  uri = "arn:aws:apigateway:${module.global.region}:lambda:path/2015-03-31/functions/arn:aws:lambda:${module.global.region}:${data.aws_caller_identity.current.account_id}:function:${aws_lambda_function.lambda_demo.function_name}/invocations"
+  integration_http_method = "POST"
+}
+
+resource "aws_api_gateway_deployment" "lambda_demo_deployment_dev" {
+  depends_on = [
+    "aws_api_gateway_method.lambda_demo_api_method",
+    "aws_api_gateway_integration.lambda_demo_api_method-integration"
+  ]
+  rest_api_id = "${aws_api_gateway_rest_api.lambda_demo_api.id}"
+  stage_name = "dev"
+}
+
+resource "aws_api_gateway_deployment" "lambda_demo_deployment_prod" {
+  depends_on = [
+    "aws_api_gateway_method.lambda_demo_api_method",
+    "aws_api_gateway_integration.lambda_demo_api_method-integration"
+  ]
+  rest_api_id = "${aws_api_gateway_rest_api.lambda_demo_api.id}"
+  stage_name = "api"
+}
+
+output "dev_url" {
+  value = "https://${aws_api_gateway_deployment.lambda_demo_deployment_dev.rest_api_id}.execute-api.${module.global.region}.amazonaws.com/${aws_api_gateway_deployment.lambda_demo_deployment_dev.stage_name}"
+}
+
+output "prod_url" {
+  value = "https://${aws_api_gateway_deployment.lambda_demo_deployment_prod.rest_api_id}.execute-api.${module.global.region}.amazonaws.com/${aws_api_gateway_deployment.lambda_demo_deployment_prod.stage_name}"
+}
+
+data "aws_caller_identity" "current" {}
 
 data "aws_ssm_parameter" "secret_read" {
   name  = "TEST_SECRET"
